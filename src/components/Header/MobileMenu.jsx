bef38fddfd4a1a175a89e10711bfa05c10d9ed77
khatia <130936157+khatias@users.noreply.github.com>
2025-01-12 00:26:18 -0500

    Added authentication status toggle with icons in mobile menu: - Displayed login/logout icons based on authentication status - Added logout functionality and redirect to login page

--- src/components/Header/MobileMenu.jsx
+++ src/components/Header/MobileMenu.jsx
@@ -1,12 +1,17 @@
 import { MenuIcon } from "@heroicons/react/solid";
 import { useState } from "react";
-import MarvelLogo from '../../assets/marvelLogo.svg'
+import MarvelLogo from "../../assets/marvelLogo.svg";
 import React from "react";
 import Drawer from "./Drawer";
-import Search from "./Search";
+import { LoginIcon, LogoutIcon, UserIcon } from "@heroicons/react/outline"; // Added UserIcon
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const MobileMenu = () => {
   const [isSideNavOpen, setSideNavOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const isAuthenticated = localStorage.getItem("authTokenMarvel");
 
   const handleOpenSideNav = () => {
     setSideNavOpen(true);
@@ -17,15 +22,42 @@ const MobileMenu = () => {
     setSideNavOpen(false);
     console.log("closed");
   };
+
+  const handleLogout = () => {
+    localStorage.removeItem("authTokenMarvel");
+    navigate("/login");
+  };
+
   return (
     <div className="mobile-nav-container">
-      <button className="burger-button" onClick={handleOpenSideNav}>
+      <button className="mobile-nav-button" onClick={handleOpenSideNav}>
         <MenuIcon className="burger-icon icon" />
       </button>
 
       <img src={MarvelLogo} alt="Marvel Logo" />
       <Drawer isOpen={isSideNavOpen} onclose={handleCloseSideNav} />
-      <Search/>
+
+      <div className="mobile-buttons">
+      {isAuthenticated && (
+          <div className="user-info">
+            <UserIcon className="icon text-green-500" />
+          </div>
+        )}
+        {isAuthenticated ? (
+          <button
+            className="mobile-nav-button logout-btn"
+            onClick={handleLogout}
+            title="Logout"
+          >
+            <LogoutIcon className="icon" />
+          </button>
+        ) : (
+          <Link to="/login" title="Login">
+            <LoginIcon className="icon text-blue-500" />
+          </Link>
+        )}
+ 
+      </div>
     </div>
   );
 };
